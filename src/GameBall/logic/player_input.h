a954d884ff6b0be59ae17508f2c2e91a31e19e7c
PeppaKing8 <wxb23@mails.tsinghua.edu.cn>
2023-12-31 15:49:54 +0800

    [feat] use key 1,2,3,4 to make radius/mass higher/lower

--- src/GameBall/logic/player_input.h
+++ src/GameBall/logic/player_input.h
@@ -12,14 +12,11 @@ struct PlayerInput {
   bool move_left{false};
   bool move_right{false};
   bool brake{false};
-<<<<<<< Updated upstream
-=======
   bool sprint{false};
   bool ball_large{false};
   bool ball_small{false};
   bool mass_large{false};
   bool mass_small{false};
->>>>>>> Stashed changes
   glm::vec3 orientation{0.0f, 0.0f, 1.0f};
 };
 
