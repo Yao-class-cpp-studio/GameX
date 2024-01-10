e9d9d9ebc26fb85636d5d9e50c285d1ed2e597c8
Prince-Polo <144091205+Prince-Polo@users.noreply.github.com>
2024-01-10 22:01:46 +0800

    Update player_input.h

--- src/GameBall/logic/player_input.h
+++ src/GameBall/logic/player_input.h
@@ -11,7 +11,15 @@ struct PlayerInput {
   bool move_backward{false};
   bool move_left{false};
   bool move_right{false};
+  bool speed{false};
+  bool jump{false};
+  bool large{false};
+  bool small{false};
+  bool heavy{false};
+  bool light{false};
   bool brake{false};
+  bool no_damping{false};
+  bool damping{false};
   glm::vec3 orientation{0.0f, 0.0f, 1.0f};
 };
 
