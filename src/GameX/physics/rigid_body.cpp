2992b94b17f09077e944e1a546085b215fa577ff
Rita1218 <145887023+Rita1218@users.noreply.github.com>
2024-01-15 16:07:46 +0800

    Update rigid_body.cpp

--- src/GameX/physics/rigid_body.cpp
+++ src/GameX/physics/rigid_body.cpp
@@ -1,9 +1,14 @@
 #include "GameX/physics/rigid_body.h"
+#include <iostream>
 
 namespace GameX::Physics {
 
 void RigidBody::Update(float delta_time) {
   position += velocity * delta_time;
+  if (position.y < -20.0) {
+    std::cout << "GAME OVER" << std::endl;
+    exit(1);
+  }
   orientation = Base::Rotate(angular_velocity * delta_time) * orientation;
 }
 
