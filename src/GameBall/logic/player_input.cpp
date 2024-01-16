925efbe81d474005b81d28fec1c4ad04d388d9e3
Rita1218 <145887023+Rita1218@users.noreply.github.com>
2024-01-16 11:48:45 +0800

    Update player_input.cpp

--- src/GameBall/logic/player_input.cpp
+++ src/GameBall/logic/player_input.cpp
@@ -12,10 +12,16 @@ PlayerInput PlayerInputController::GetInput() {
   input_.move_backward = (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS);
   input_.move_left = (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS);
   input_.move_right = (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS);
-  input_.brake = (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS);
-  input_.jump = (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS);
+  input_.brake = (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS);
+  input_.jump = (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS);
   input_.quit = (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS);
   input_.reset = (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS);
+  input_.move_forward1 = (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS);
+  input_.move_backward1 = (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS);
+  input_.move_left1 = (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS);
+  input_.move_right1 = (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS);
+  input_.brake1 = (glfwGetKey(window, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS);
+  input_.jump1 = (glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS);
   auto camera_controller = app_->CameraController();
   auto pitch_yaw = camera_controller->GetPitchYaw();
   auto pitch = pitch_yaw.x;
